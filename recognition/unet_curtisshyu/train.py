71a1c00d75c77ff64dfb999da4a05a89f25c599f
Curtis Shyu <c.shyu@student.uq.edu.au>
2025-10-29 17:19:13 +1000

    Fixed import statements

--- recognition/unet_curtisshyu/train.py
+++ recognition/unet_curtisshyu/train.py
@@ -1,5 +1,5 @@
-from .modules import UNet
-from .utils import param_check
+from recognition.unet_curtisshyu.modules import UNet
+from recognition.unet_curtisshyu.utils import param_check
 
 model = UNet(n_channels=1, n_classes=1)
 _, params = param_check(model)
