9a0b3da31f05f9600715c129352fd728c079e9c0
Curtis Shyu <c.shyu@student.uq.edu.au>
2025-10-29 17:05:52 +1000

    Added a param sanity check in utils and made some notes in readme

---
+++ recognition/unet_curtisshyu/utils.py
@@ -0,0 +1,26 @@
+"""
+Contains utility functions for U-Net architecture
+- Dice Coefficient
+- Loss Functions
+- Testing Param Count
+"""
+
+from recognition.unet_curtisshyu.modules import UNet
+import torch
+
+def param_check(model, input_shape = (1, 1, 128, 128)):
+    # Dummy inputs
+    x = torch.randn(*input_shape)
+
+    model.eval()
+
+    with torch.no_grad():
+        y = model(x)
+    params_count = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    print(f"Input shape: {x.shape}")
+    print(f"Output shape: {y.shape}")
+    print(f"Trainable parameters: {params_count}")
+
+    return y, params_count # Expect to get x shape = y shape
+
